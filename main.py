87c354c62a2c1d6c88d53f09a24814c6783e4c04
Niroshan2812 <niroshan9812@gmail.com>
2023-01-01 00:23:52 +0530

    second

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+def bsearch(list, val):
+   list_size = len(list) - 1
+   idx0 = 0
+   idxn = list_size
+    # Find the middle most value
+   while idx0 <= idxn:
+      midval = (idx0 + idxn)// 2
+      if list[midval] == val:
+         return midval
+    # Compare the value the middle most value
+   if val > list[midval]:
+      idx0 = midval + 1
+   else:
+      idxn = midval - 1
+   if idx0 > idxn:
+      return None
+# Initialize the sorted list
+list = [2,7,19,34,53,72]
+
+# Print the search result
+print(bsearch(list,72))
+print(bsearch(list,11))
+
+
 
 #Big-Theta Notation
 def fistSimpleFizzBuzz():
@@ -14,5 +38,5 @@ def fistSimpleFizzBuzz():
             print(num)
 
 
-fistSimpleFizzBuzz()
+bsearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],5)
 
