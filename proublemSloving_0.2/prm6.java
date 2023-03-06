0ebed5045a36a863861a0441dbdd6aa064f4cb74
Niroshan2812 <niroshan9812@gmail.com>
2023-03-06 23:25:36 +0530

    proublem 7

---
+++ proublemSloving_0.2/prm6.java
@@ -0,0 +1,12 @@
+/*
+ * You have been given a list of integers. Write a Java function to return the second smallest integer in the list. 
+ * If there is no second smallest integer (e.g., all the integers in the list are the same), then the function should return -1.
+ */
+
+
+public class prm6 {
+    
+    public static void main(String[] args) {
+        
+    }
+}
