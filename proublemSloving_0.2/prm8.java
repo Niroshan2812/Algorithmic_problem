53a573e2a4cc98b88f79b2de6f527b722fa04a46
Niroshan2812 <niroshan9812@gmail.com>
2023-03-11 23:16:11 +0530

    proublem 8

---
+++ proublemSloving_0.2/prm8.java
@@ -0,0 +1,13 @@
+public class prm8 {
+
+    public static void pivotalgorithem(){
+        int[] arr = {2,4,5,6,8,33,4,5,9,12,};
+        int highIndex = arr.length;
+        int lft = 0;
+        int right = 0;
+    
+    }
+    public static void main(String[] args) {
+        
+    }
+}
