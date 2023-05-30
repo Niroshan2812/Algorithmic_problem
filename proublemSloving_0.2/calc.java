a72b9c10e537fd781ec5b546fea9b2ed8f20e6b4
Niroshan2812 <niroshan9812@gmail.com>
2023-05-31 03:12:44 +0530

    fib des

--- proublemSloving_0.2/calc.java
+++ proublemSloving_0.2/calc.java
@@ -1,4 +1,25 @@
 //fibonachi number calculation
+//The proublem is when you call for example 50 as the input it gain lot time to calculation 
+/*
+ *          7
+ *         /\
+ *        /  \
+ *       6    4
+ *      /\    /\ 
+ *     /  \   3 2
+ *    5    4  /\
+ *   /\    /\ 2 1
+ *  3  2   3 1
+ * /\      /\
+ * 2 1    2  1
+ * 
+ * if the node 2 or 1 the return is 1 
+ * Then calculate the numbers bottom to top 
+ * 
+ */ 
+
+
+
 
 public class calc {
     public static void main(String[] args) {
